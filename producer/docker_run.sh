0c5753497bc87d561cd7cda2269d38dea6d3c9b6
Hendris Koya <hkoya@uolinc.com>
2020-04-19 22:16:02 -0300

    configured cloud bus and script do test health check

--- producer/docker_run.sh
+++ producer/docker_run.sh
@@ -5,5 +5,7 @@ docker run \
   --network spring-boot-cloud \
   -p 9091:9091 \
   -e "SPRING_CLOUD_CONFIG_URI=http://config-server:8888" \
+  -e "EUREKA_CLIENT_SERVICE_URL=http://discovery:8761/eureka" \
+  -e "RABBITMQ_HOST=rabbitmq" \
   --rm \
   producer:latest
