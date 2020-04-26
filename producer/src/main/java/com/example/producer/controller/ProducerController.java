1041687dabce1aa512de346a458ca639f94db090
Hendris Koya <hkoya@uolinc.com>
2020-04-26 20:18:17 -0300

    added log to api

--- producer/src/main/java/com/example/producer/controller/ProducerController.java
+++ producer/src/main/java/com/example/producer/controller/ProducerController.java
@@ -1,12 +1,16 @@
 package com.example.producer.controller;
 
 import com.example.producer.properties.ApplicationProperties;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ProducerController {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ProducerController.class);
 
     private final ApplicationProperties applicationProperties;
 
@@ -16,6 +20,7 @@ public class ProducerController {
 
     @GetMapping("/properties")
     public ResponseEntity<PropertiesResponse> getProperties() {
+        LOG.info("Endpoint '/properties' was called at {}", System.currentTimeMillis());
         return ResponseEntity.ok(new PropertiesResponse(applicationProperties.getMessage()));
     }
 }
